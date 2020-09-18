00f7113a7df21eab4d5129aa119f63e3e5fc1d80
zjn0613 <1160468940@qq.com>
2020-09-18 11:41:55 +0800

    modify citycollectionwidget.cpp

--- src/citycollectionwidget.cpp
+++ src/citycollectionwidget.cpp
@@ -59,8 +59,8 @@ CityCollectionWidget::CityCollectionWidget(QWidget *parent) :
     setProperty("blurRegion", QRegion(path.toFillPolygon().toPolygon()));
     this->setStyleSheet("QWidget{border:none;border-radius:6px;}");
     this->setWindowIcon(QIcon::fromTheme("indicator-china-weather", QIcon(":/res/control_icons/indicator-china-weather.png")) ); //set taskbar icon
-    this->setWindowTitle(tr("Kylin Weather"));
-//    this->setWindowIconText("麒麟天气");
+    this->setWindowTitle(tr("麒麟天气"));
+
     QRect availableGeometry = qApp->primaryScreen()->availableGeometry();
     this->move((availableGeometry.width()-this->width())/2, (availableGeometry.height()-this->height())/2);
 
