17ce57543c703827323cd469acb68c41aae4d388
Andrew <sxshdor@163.com>
2021-01-05 16:17:20 +0800

    更新了ts文件

--- translations/indicator-china-weather_bo.ts
+++ translations/indicator-china-weather_bo.ts
@@ -12,7 +12,8 @@
 <context>
     <name>CityCollectionWidget</name>
     <message>
-        <location filename="../src/citycollectionwidget.cpp" line="61"/>
+        <location filename="../src/citycollectionwidget.cpp" line="76"/>
+        <location filename="../src/citycollectionwidget.cpp" line="82"/>
         <source>Kylin Weather</source>
         <translation type="unfinished"></translation>
     </message>
@@ -28,44 +29,45 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/mainwindow.cpp" line="41"/>
-        <location filename="../src/mainwindow.cpp" line="301"/>
+        <location filename="../src/mainwindow.cpp" line="44"/>
+        <location filename="../src/mainwindow.cpp" line="95"/>
+        <location filename="../src/mainwindow.cpp" line="405"/>
         <source>Kylin Weather</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="60"/>
+        <location filename="../src/mainwindow.cpp" line="109"/>
         <source>Open Kylin Weather</source>
         <translation type="unfinished"></translation>
     </message>
-	<message>
-        <location filename="../src/mainwindow.cpp" line="69"/>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="73"/>
         <source>Add City</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="74"/>
-        <source>About</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="61"/>
+        <location filename="../src/mainwindow.cpp" line="110"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="104"/>
+        <location filename="../src/mainwindow.cpp" line="142"/>
         <source>Network not connected</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="251"/>
+        <location filename="../src/mainwindow.cpp" line="355"/>
         <source>Incorrect access address</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="253"/>
+        <location filename="../src/mainwindow.cpp" line="357"/>
         <source>Network error code:%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -78,15 +80,85 @@
     </message>
 </context>
 <context>
+    <name>cityaddition</name>
+    <message>
+        <location filename="../src/cityaddwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>citycollectionitem</name>
+    <message>
+        <location filename="../src/citycollectionitem.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>citycollectionwidget</name>
+    <message>
+        <location filename="../src/citycollectionwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>information</name>
+    <message>
+        <location filename="../src/informationwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
-        <location filename="../src/main.cpp" line="71"/>
+        <location filename="../src/main.cpp" line="72"/>
         <source>KylinWeather</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="75"/>
+        <location filename="../src/main.cpp" line="76"/>
         <source>show indicator-china-weather test</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>menuModule</name>
+    <message>
+        <location filename="../src/menumodule.cpp" line="22"/>
+        <source>Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="24"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="26"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="28"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="187"/>
+        <source>indicator china weather</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="229"/>
+        <source>Version: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="233"/>
+        <source>Support: support@kylinos.cn</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- translations/indicator-china-weather_zh_CN.ts
+++ translations/indicator-china-weather_zh_CN.ts
@@ -12,7 +12,8 @@
 <context>
     <name>CityCollectionWidget</name>
     <message>
-        <location filename="../src/citycollectionwidget.cpp" line="61"/>
+        <location filename="../src/citycollectionwidget.cpp" line="76"/>
+        <location filename="../src/citycollectionwidget.cpp" line="82"/>
         <source>Kylin Weather</source>
         <translation>麒麟天气</translation>
     </message>
@@ -28,45 +29,50 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/mainwindow.cpp" line="41"/>
-        <location filename="../src/mainwindow.cpp" line="301"/>
+        <location filename="../src/mainwindow.cpp" line="44"/>
+        <location filename="../src/mainwindow.cpp" line="95"/>
+        <location filename="../src/mainwindow.cpp" line="405"/>
         <source>Kylin Weather</source>
         <translation>麒麟天气</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="60"/>
+        <location filename="../src/mainwindow.cpp" line="109"/>
         <source>Open Kylin Weather</source>
         <translation>打开麒麟天气</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="69"/>
+        <location filename="../src/mainwindow.cpp" line="73"/>
         <source>Add City</source>
         <translation>增加城市</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="74"/>
         <source>About</source>
-        <translation>关于</translation>
+        <translation type="vanished">关于</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="61"/>
+        <location filename="../src/mainwindow.cpp" line="110"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="104"/>
+        <location filename="../src/mainwindow.cpp" line="142"/>
         <source>Network not connected</source>
         <translation>网络未连接</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="251"/>
+        <location filename="../src/mainwindow.cpp" line="355"/>
         <source>Incorrect access address</source>
         <translation>访问地址异常</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="253"/>
+        <location filename="../src/mainwindow.cpp" line="357"/>
         <source>Network error code:%1</source>
         <translation>网络错误代码：%1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -78,16 +84,86 @@
     </message>
 </context>
 <context>
+    <name>cityaddition</name>
+    <message>
+        <location filename="../src/cityaddwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>citycollectionitem</name>
+    <message>
+        <location filename="../src/citycollectionitem.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>citycollectionwidget</name>
+    <message>
+        <location filename="../src/citycollectionwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>information</name>
+    <message>
+        <location filename="../src/informationwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
-        <location filename="../src/main.cpp" line="71"/>
+        <location filename="../src/main.cpp" line="72"/>
         <source>KylinWeather</source>
         <translation>KylinWeather</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="75"/>
+        <location filename="../src/main.cpp" line="76"/>
         <source>show indicator-china-weather test</source>
         <translation>show indicator-china-weather test</translation>
+    </message>
+</context>
+<context>
+    <name>menuModule</name>
+    <message>
+        <location filename="../src/menumodule.cpp" line="22"/>
+        <source>Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="24"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="26"/>
+        <source>About</source>
+        <translation type="unfinished">关于</translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="28"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="187"/>
+        <source>indicator china weather</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="229"/>
+        <source>Version: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/menumodule.cpp" line="233"/>
+        <source>Support: support@kylinos.cn</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
