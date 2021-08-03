21bb8af041d40fb83513d4fafe00e58db9b32b22
zjn0613 <1160468940@qq.com>
2021-08-03 14:53:40 +0800

    添加copyright

--- src/daemondbus.cpp
+++ src/daemondbus.cpp
@@ -1,10 +1,12 @@
 /*
  * Copyright (C) 2020, KylinSoft Co., Ltd.
  *
- * This program is free software: you can redistribute it and/or modify
+ * Authors:
+ *  Kobe Lee    lixiang@kylinos.cn/kobe24_lixiang@126.com
+ *
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "daemondbus.h"
